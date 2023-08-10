45b48e733baa6bcdf285a94c0f1db566b56e32c2
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2023-08-10 18:36:35 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,202 @@
+# Passbolt Helper
+
+<!--
+Script to make Passbolt CLI more user friendly.
+Copyright (C) 2023  Twilight Sparkle
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+Script to make [Passbolt CLI](https://github.com/passbolt/passbolt_cli) more user friendly.
+
+Due to this script parsing potentially unstable output, this is only verified to work with `master` branch of
+`passbolt_cli` as of July 26, 2023 with commit `289017e`.
+
+Other versions may work, and if it does not, it should not break anything. But it may show you badly formatted or
+incorrect data.
+
+If there is a more elegant way of getting and parsing the data from `passbolt_cli` that I have missed, let me know :)
+
+## Setup
+
+- Install [passbolt_cli](https://github.com/passbolt/passbolt_cli)
+- Import the required keys into gpg
+
+  ```bash
+  # Import the public key for your Passbolt server
+  passbolt server-key --domain=https://passbolt.example.com | gpg --import
+
+  # Import your Passbolt account private key
+  gpg --import /path/to/passbolt_private.asc
+
+  # Get the key IDs for your keys
+  # (The ID is the 40-character long random string)
+  gpg --list-keys
+
+  # Set the trust of the server key to ultimate
+  gpg --edit-key <server_key_id>
+  gpg> trust
+  Please decide how far you trust this user to correctly verify other users keys
+  Your decision? 5
+  Do you really want to set this key to ultimate trust? (y/N) y
+  gpg> quit
+
+  # Repeat the previous step to set the trust of your key to ultimate
+  ```
+
+- Create `passbolt_cli` config directory
+
+  ```bash
+  mkdir -p ~/.config/passbolt
+  ```
+
+- Create `passbolt_cli` config file `~/.config/passbolt/config.json`
+
+  ```json
+  {
+    "domain": {
+      "baseUrl": "https://passbolt.example.com",
+      "publicKey": {
+        "fingerprint": "ServerGpgKeyID"
+      }
+    },
+    "user": {
+      "firstname": "YourFirstName",
+      "lastname": "YourLastName",
+      "email": "YourEmail@address",
+      "privateKey": {
+        "fingerprint": "YourGpgKeyID"
+      }
+    },
+    "mfa": {
+      "providers": ["totp"]
+    },
+    "agentOptions": {
+      "rejectUnauthorized": true
+    }
+  }
+  ```
+
+  If you are using a YubiKey for 2FA, see <https://github.com/passbolt/passbolt_cli#mfa-preferences>.
+
+- Test `passbolt_cli` to make sure it's working
+
+  ```bash
+  passbolt find
+  ```
+
+  Should return a list of all your passbolt entries.
+
+- `git clone` or
+  <a href="https://raw.githubusercontent.com/Twi1ightSparkle/passbolt-helper/main/passbolt.sh" download>download</a>
+  the script.
+- Add an alias to your `.bashrc`/`.zshrc`/etc file
+
+  ```bash
+  alias pb='/path/to/passbolt-helper/passbolt.sh'
+  ```
+
+- Set up your Passbolt. The way `passbolt_cli` works is really dumb. First, it does not have any way of searching, it
+  can only return all your entries. Seconds, and relevant here, `passbolt_cli` only returns your Passbolt entries in a
+  space separated list. So the only way to parse it programmatically is to count characters. To make this possible, you
+  must add a new password to your Passbolt account where the entry name, the username, and the URL is any string that is
+  **exactly 200 characters** long. Here is one for convenient copy-pasting
+  `ZZZZdummyEntryIgnoreThisVeryLongStringqTWSG1TqYTyhmceL51ncBPv873TQCmCVl3SraSr84Xw2a3GFXOiQqgeFw66rtRD35bZ771anUoUtBPWVyldpWHeZwt5M4od4LsS4P85kzIraUDRj3pfQPMjvYVgzkjaRPGZeHoTzVmS1J7lr0TzJcu4CZXXYgC7SNE`
+
+- Optionally change program defaults by setting one or more of of these in your .bashrc/.zshrc/etc file
+
+  ```bash
+  export PASSBOLT_CLI_HELPER_COPY="true"
+  export PASSBOLT_CLI_HELPER_SHOW_DESC="true"
+  export PASSBOLT_CLI_HELPER_SHOW_PW="true"
+  ```
+
+## Errors
+
+If you get an error from `passbolt_cli` complaining about pinentry, install pinentry for your OS. On my Mac, I had to
+install `pinentry-mac`, then add `pinentry-program /usr/local/bin/pinentry-mac` to `~/.gnupg/gpg-agent.conf`.
+
+## Example
+
+```bash
+$ pb --help
+Simple helper script to make Passbolt CLI a bit more usable.
+
+Due to the really dumb way passbolt-cli returns the list of your
+credentials, you must create an entry in your Passbolt with any string of
+exactly 200 characters in the name, username, and URL field 🤦‍♀️
+
+Usage: ./passbolt.sh [options] [search] [index]
+
+Options:
+    -c, --copy          Copy username and password to the system clipboard.
+    -d, --description   Print the description to the console.
+    -h, --help          This help text.
+    -p, --password      Print the password to the console.
+        --verbose       Show verbose debug information. Printed to STDERR.
+
+search: Search for something in Passbolt. Optional, script will ask you
+        if not set.
+index:  If your search returns more than one result, and index is not
+        set, you will be presented a list to choose an entry from.
+        Set index to pre-select the result from the list.
+
+Settings. Export environment variable with the value \"true\"
+(including quotes) to enable the option. When one of these is set, they negate
+the related option if set.
+    - PASSBOLT_CLI_HELPER_COPY:         Automatically copy the username and
+                                        password to system clipboard.
+    - PASSBOLT_CLI_HELPER_SHOW_DESC:    Print the description by default.
+    - PASSBOLT_CLI_HELPER_SHOW_PW:      Print the password by default.
+
+$ pb notfound
+No entries found for notfound
+
+$ pb 'demo entry'
+
+Details for entry   Demo Entry
+Username:           twilight
+Password:           <hidden>
+URL:                https://example.com
+Last modified:      2023-07-25T14:43:17+00:00
+Passbolt link:      https://passbolt.example.com/app/passwords/view/e6e04cba-9ba2-4965-8425-cce7b7003a0f
+Description:        <hidden>
+
+$ pb --password demo
+1: Demo Entry, twilight (https://example.com)
+2: Entry 1, user1
+3: Entry 2, user2
+Choose entry number: 1
+
+Details for entry   Demo Entry
+Username:           twilight
+Password:           ThisIsASecret
+URL:                https://example.com
+Last modified:      2023-07-25T14:43:17+00:00
+Passbolt link:      https://passbolt.example.com/app/passwords/view/e6e04cba-9ba2-4965-8425-cce7b7003a0f
+Description:        <hidden>
+
+$ pb --password --description --copy demo 1
+
+Details for entry   Demo Entry
+Username:           twilight
+Password:           ThisIsASecret
+URL:                https://example.com
+Last modified:      2023-07-25T14:43:17+00:00
+Passbolt link:      https://passbolt.example.com/app/passwords/view/e6e04cba-9ba2-4965-8425-cce7b7003a0f
+Description:
+And, this is a description
+
+The username has been copied to your clipboard. Press enter to copy the password
+```
---
+++ passbolt.sh
@@ -0,0 +1,408 @@
+#!/bin/bash
+
+# Script to make Passbolt CLI more user friendly.
+# Copyright (C) 2023  Twilight Sparkle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+dumb="Due to the really dumb way passbolt-cli returns the list of your
+credentials, you must create an entry in your Passbolt with any string of
+exactly 200 characters in the name, username, and URL field 🤦‍♀️"
+
+# Print script help
+printHelp() {
+    cat <<EOF
+Simple helper script to make Passbolt CLI a bit more usable.
+
+$dumb
+
+Usage: ./passbolt.sh [options] [search] [index]
+
+Options:
+    -c, --copy          Copy username and password to the system clipboard.
+    -d, --description   Print the description to the console.
+    -h, --help          This help text.
+    -p, --password      Print the password to the console.
+        --verbose       Show verbose debug information. Printed to STDERR.
+
+search: Search for something in Passbolt. Optional, script will ask you
+        if not set.
+index:  If your search returns more than one result, and index is not
+        set, you will be presented a list to choose an entry from.
+        Set index to pre-select the result from the list.
+
+Settings. Export environment variable with the value \"true\"
+(including quotes) to enable the option. When one of these is set, they negate
+the related option if set.
+    - PASSBOLT_CLI_HELPER_COPY:         Automatically copy the username and
+                                        password to system clipboard.
+    - PASSBOLT_CLI_HELPER_SHOW_DESC:    Print the description by default.
+    - PASSBOLT_CLI_HELPER_SHOW_PW:      Print the password by default.
+EOF
+}
+
+# Parse command line parameters
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        # options
+        -h | --help)        printHelp;                                  exit 0  ;;
+        -c | --copy)        export OPTION_PB_HELPER_COPY="true";        shift   ;;
+        -d | --description) export OPTION_PB_HELPER_DESC="true";        shift   ;;
+        -p | --password)    export OPTION_PB_HELPER_PASSWORD="true";    shift   ;;
+             --verbose)     export OPTION_PB_HELPER_VERBOSE="true";     shift   ;;
+        # :cry:, but don't want any libraries or dependencies for this
+        -cd)  export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true";                                          shift ;;
+        -cp)  export OPTION_PB_HELPER_COPY="true";                                      export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -cdp) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -cpd) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -dc)  export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true";                                          shift ;;
+        -dp)                                       export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -dcp) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -dpc) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -pc)  export OPTION_PB_HELPER_COPY="true";                                      export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -pd)                                       export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -pcd) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        -pdc) export OPTION_PB_HELPER_COPY="true"; export OPTION_PB_HELPER_DESC="true"; export OPTION_PB_HELPER_PASSWORD="true"; shift ;;
+        # Catch all invalid options
+        -*)                 echo "Unknown option $1.";                  exit 1  ;;
+        # Save positional arguments
+        *)                  POSITIONAL_ARGS+=("$1");                    shift   ;;
+    esac
+done
+# Restore positional arguments
+set -- "${POSITIONAL_ARGS[@]}"
+
+# Print a header for debug information
+# Params:
+# 1: Header name
+printDebugHeader() {
+    if [ "$OPTION_PB_HELPER_VERBOSE" == "true" ]; then
+        local header
+        header="$1"
+        >&2 echo -e "\n-------------------Debug info for $header-------------------"
+    fi
+}
+
+# Print a variable as a debug line or debug header
+# Params:
+# 1: The variable name to print without $ or the header if $2 = header
+printDebugVar() {
+    if [ "$OPTION_PB_HELPER_VERBOSE" == "true" ]; then
+        local dots
+        local length
+        local numDots
+        local var
+
+        var="$1"
+        length="${#var}"
+        numDots="$((40-length))"
+        dots=""
+        for (( i=0; i<="$numDots"; ++i)); do dots+="."; done
+        >&2 echo "DEBUG: \$$var$dots${!var}.........."
+    fi
+}
+
+# Ask for a search it not set on the command line
+searchPhrase="$1"
+if [ -z "$searchPhrase" ]; then
+    read -rp "Search: " searchPhrase
+fi
+
+# Set index if provided
+multiIndex="$2"
+
+# Check what OS the system is running.
+# Returns String. One of Darwin, LinuxWayland, or LinuxX11. Or Number 1 if unable to detect
+checkOs() {
+    local unameRes
+    unameRes="$(uname)"
+    printDebugHeader "function checkOs"
+    printDebugVar "unameRes"
+    
+    case "$unameRes" in
+        Darwin)
+            echo "Darwin";;
+        Linux)
+            local windowManager
+            # shellcheck disable=SC2046
+            windowManager="$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')"
+            printDebugVar "windowManager"
+            if echo "$windowManager" | grep wayland 2>/dev/null; then
+                echo "LinuxWayland"
+            else
+                echo "LinuxX11"
+            fi;;
+        *)
+            echo 1
+    esac
+}
+
+# Defaults
+COPY_USER_PASS="false"
+SHOW_DESCRIPTION="false"
+SHOW_PASSWORD="false"
+
+# Negation. I'm sure there is a more elegant way of doing this, but :shrug:
+if [ "$PASSBOLT_CLI_HELPER_COPY" == "true" ] && [ -z "$OPTION_PB_HELPER_COPY" ]; then COPY_USER_PASS="true"; fi
+if [ "$PASSBOLT_CLI_HELPER_SHOW_DESC" == "true" ] && [ -z "$OPTION_PB_HELPER_DESC" ]; then SHOW_DESCRIPTION="true"; fi
+if [ "$PASSBOLT_CLI_HELPER_SHOW_PW" == "true" ] && [ -z "$OPTION_PB_HELPER_PASSWORD" ]; then SHOW_PASSWORD="true"; fi
+
+if [ "$PASSBOLT_CLI_HELPER_COPY" == "true" ] && [ "$OPTION_PB_HELPER_COPY" == "true" ]; then COPY_USER_PASS="false"; fi
+if [ "$PASSBOLT_CLI_HELPER_SHOW_DESC" == "true" ] && [ "$OPTION_PB_HELPER_DESC" == "true" ]; then SHOW_DESCRIPTION="false"; fi
+if [ "$PASSBOLT_CLI_HELPER_SHOW_PW" == "true" ] && [ "$OPTION_PB_HELPER_PASSWORD" == "true" ]; then SHOW_PASSWORD="false"; fi
+
+if [ ! "$PASSBOLT_CLI_HELPER_COPY" == "true" ] && [ -z "$OPTION_PB_HELPER_COPY" ]; then COPY_USER_PASS="false"; fi
+if [ ! "$PASSBOLT_CLI_HELPER_SHOW_DESC" == "true" ] && [ -z "$OPTION_PB_HELPER_DESC" ]; then SHOW_DESCRIPTION="false"; fi
+if [ ! "$PASSBOLT_CLI_HELPER_SHOW_PW" == "true" ] && [ -z "$OPTION_PB_HELPER_PASSWORD" ]; then SHOW_PASSWORD="false"; fi
+
+if [ ! "$PASSBOLT_CLI_HELPER_COPY" == "true" ] && [ "$OPTION_PB_HELPER_COPY" == "true" ]; then COPY_USER_PASS="true"; fi
+if [ ! "$PASSBOLT_CLI_HELPER_SHOW_DESC" == "true" ] && [ "$OPTION_PB_HELPER_DESC" == "true" ]; then SHOW_DESCRIPTION="true"; fi
+if [ ! "$PASSBOLT_CLI_HELPER_SHOW_PW" == "true" ] && [ "$OPTION_PB_HELPER_PASSWORD" == "true" ]; then SHOW_PASSWORD="true"; fi
+
+# Check if able to determine OS
+if [ "$COPY_USER_PASS" == "true" ]; then
+    operatingSystem="$(checkOs)"
+    if [ "$operatingSystem" == 1 ]; then
+        echo "Unable to detect operating system. Update the script or disable copy to clipboard";
+        exit 1
+    fi
+fi
+
+# Print debug vars
+printDebugHeader "input variables and settings"
+debugVars=(searchPhrase multiIndex operatingSystem PASSBOLT_CLI_HELPER_COPY OPTION_PB_HELPER_COPY COPY_USER_PASS
+    PASSBOLT_CLI_HELPER_SHOW_DESC OPTION_PB_HELPER_DESC SHOW_DESCRIPTION PASSBOLT_CLI_HELPER_SHOW_PW
+    OPTION_PB_HELPER_PASSWORD SHOW_PASSWORD)
+for v in "${debugVars[@]}"; do printDebugVar "$v" ; done
+
+# Make sure required programs are installed
+checkRequiredPrograms() {
+    local program
+    local programs
+    local missing
+
+    programs=(bash echo gpg grep jq passbolt read sed xargs)
+
+    if [ "$COPY_USER_PASS" == "true" ]; then
+        case "$operatingSystem" in
+            Darwin)         programs+=(pbcopy);;
+            LinuxWayland)   programs+=(wl-copy);;
+            LinuxX11)       programs+=(xclip);;
+        esac
+    fi
+
+    missing=""
+    for program in "${programs[@]}"; do
+        if ! hash "$program" &>/dev/null; then
+            missing+="\n- $program "
+        fi
+    done
+
+    if [ -n "$missing" ]; then
+        echo -e "Some required programs are missing on this system. Please install:$missing"
+        exit 1
+    fi
+}
+
+checkRequiredPrograms
+
+# Copy a string to the system clipboard
+# Params:
+# 1: The string to copy
+copy() {
+    local string
+    string="$1"
+
+    printDebugHeader "function copy"
+    printDebugVar "string"
+
+    case $operatingSystem in
+        Darwin)         echo "$string" | tr -d '\n' | pbcopy;;
+        LinuxWayland)   wl-copy "$string";;
+        LinuxX11)       xclip -i -selection clipboard -t "$string";;
+        *)
+            echo "Unable to copy to system clipboard."
+            return 1
+    esac
+}
+
+# Takes one line returned from passbolt find, parses it, and returns it as a usable string
+# Params:
+# 1: The line from Passbolt
+# 2: Format to return the string in
+#   all:    name;username;uri;modified;uuid
+#   short:  name, username. If URL is set, append (url)
+# Returns:  Formatted string
+splitLine() {
+    local input
+    local resultMode
+    local string
+    local inputName
+    local username
+    local uri
+    local modified
+    local uuid
+    input="$1"
+
+    resultMode="$2"
+    inputName="$(echo "${input:0:200}" | xargs)"
+    username="$(echo "${input:201:64}" | xargs)"
+    uri="$(echo "${input:266:200}" | xargs)"
+    modified="$(echo "${input:467:25}" | xargs)"
+    uuid="$(echo "${input:493:36}" | xargs)"
+
+    printDebugHeader "function splitLine"
+    debugVars=(input resultMode inputName username uri modified uuid)
+    for v in "${debugVars[@]}"; do printDebugVar "$v"; done
+
+    case $resultMode in
+        all)    echo "$inputName;$username;$uri;$modified;$uuid";;
+        short)
+                string="$inputName, $username"
+                [ -n "$uri" ] && string+=" ($uri)"
+                printDebugVar "string"
+                echo "$string";;
+        *)
+                echo "Invalid result mode $resultMode. Must be one of all|short"
+                exit 1
+    esac
+}
+
+# Get the encrypted password + description from Passbolt, then print it and/or copy to clipboard
+# Params:
+# 1: One line from passbolt find
+printEntry() {
+    local description
+    local decrypted
+    local encrypted
+    local password
+    local passwordStr
+    local split
+    local baseUrl
+    local entryName
+    local username
+    local uri
+    local modified
+    local uuid
+
+    split="$(splitLine "$1" "all")"
+
+    IFS=';' read -r entryName username uri modified uuid <<< "$split"
+    IFS=$' \t\n'
+
+    encrypted="$(passbolt get "$uuid")"
+    decrypted="$(echo "$encrypted" | gpg --quiet --no-tty 2>/dev/null | sed 's#\n#\\n#g')"
+    password="$(echo "$decrypted" | jq --raw-output .password)"
+    baseUrl="$(jq --raw-output .domain.baseUrl "$HOME/.config/passbolt/config.json")"
+
+    # If the entry does not have a description, the encrypted data is not JSON. It just encrypts the password as a
+    # simple string. This check should (tm) always work because Passbolt does not allow adding an entry without entering
+    # something in the password field
+    if [ "${#password}" == 0 ]; then
+        password="$decrypted"
+    else
+        description="$(echo "$decrypted" | jq --raw-output .description)"
+    fi
+
+    passwordStr="<hidden>"
+    descriptionStr="<hidden>"
+    [ "$SHOW_PASSWORD" == "true" ] && passwordStr="$password"
+    [ "$SHOW_DESCRIPTION" == "true" ] && descriptionStr="\n$description"
+
+    printDebugHeader "function printEntry"
+    debugVars=(description decrypted encrypted password baseUrl passwordStr split entryName username uri modified uuid)
+    for v in "${debugVars[@]}"; do printDebugVar "$v"; done
+
+    echo -e "
+Details for entry   $entryName
+Username:           $username
+Password:           $passwordStr
+URL:                $uri
+Last modified:      $modified
+Passbolt link:      $baseUrl/app/passwords/view/$uuid
+Description:        $descriptionStr"
+
+    if [ "$COPY_USER_PASS" == "true" ]; then
+        copy "$username"
+        read -rp "The username has been copied to your clipboard. Press enter to copy the password"
+        copy "$password"
+    fi
+}
+
+# Get all passwords from Passbolt and select one of them to process further
+getPassword() {
+    local allEntries
+    local chosenEntry
+    local counter
+    local searchResult
+    local split
+    local resultCount
+
+    # Log in to passbolt
+    if ! passbolt auth check | grep "You are already logged in." &>/dev/null; then
+        passbolt auth login
+    fi
+
+    # Fetch all entries from Passbolt. passbolt-cli does not have any native filter/search function
+    allEntries="$(passbolt find)"
+    # Convert the list to an array
+    # shellcheck disable=SC2207
+    IFS=$'\n' searchResult=($(echo "$allEntries" | grep -i "$searchPhrase"))
+    IFS=$' \t\n'
+    resultCount=${#searchResult[@]}
+
+    # Check that the user has the 200 string entry requirement
+    if [ "$resultCount" -gt 0 ] && [ ${#searchResult[0]} != 529 ]; then
+        echo "Error: $dumb"
+        exit 1
+    fi
+
+    case "$resultCount" in
+        0)
+            echo "No entries found for $searchPhrase"
+            exit 1;;
+        1)
+            printEntry "${searchResult[0]}";;
+        *)
+            # Skip the choice if a number was pre selected from the command line
+            if [ -n "$multiIndex" ]; then
+                chosenEntry="$multiIndex"
+            else
+                # List all results
+                counter=1
+                for i in "${searchResult[@]}"; do
+                    splitLine "$counter: $i" "short"
+                    ((counter++))
+                done
+                # Keep asking the user for a selection until a valid integer is entered
+                while
+                    read -rp "Choose entry number: " chosenEntry
+                    [[
+                        -z "$chosenEntry" ||                        # Empty
+                        "$chosenEntry" == *[^0-9]* ||               # Not an integer
+                        "$chosenEntry" -lt 1 ||                     # Lower than 1
+                        "$chosenEntry" -gt "${#searchResult[@]}"    # Greater than the number of results
+                    ]];
+                do
+                    true
+                done
+            fi
+
+            # Process and print the chosen entry
+            ((chosenEntry--))
+            printEntry "${searchResult[$chosenEntry]}"
+    esac
+}
+
+getPassword
